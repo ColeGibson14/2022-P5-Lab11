670586023fbcc495e3b0f21c008c1c266183dd63
Cole Gibson <Cole.Gibson@vtsd.com>
2023-01-06 11:49:59 -0500

    Finished Lab

--- Lab11avst.java
+++ Lab11avst.java
@@ -6,31 +6,53 @@
 import java.util.Scanner;
 
 
-public class Lab11avst
-{
-    public static void main(String[] args)
-    {
+public class Lab11avst {
+    public static void main(String[] args) {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
-        boolean primes[];
+        System.out.print("Enter the primes upper bound ====>>  ");
+        final int MAX = input.nextInt();
+        boolean primes[] = new boolean[MAX];
         primes = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
 
-    public static void computePrimes(boolean primes[])
-    {
+    public static void computePrimes(boolean primes[]) {
+
         System.out.println("\nCOMPUTING PRIME NUMBERS");
 
-    }
+        for (int k = 2; k < primes.length; k++) {
+            primes[k] = true;
 
-    public static void displayPrimes(boolean primes[])
-    {
-        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+        }
+        for (int h = 2; h < primes.length; h++) {
+            for (int k = h * 2; k < primes.length; k += h) {
+                primes[k] = false;
+
+            }
+        }
+
+        }
+
+    public static void displayPrimes(boolean primes[]) {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
         System.out.println();
+        int counter = 0;
+        for (int k = 2; k < primes.length; k++) {
+            if (primes[k] == true){
+                System.out.print(k + " ");
+                counter++;
+                if(counter == 15){
+                    counter = 0;
+                    System.out.println();
+                }
+
+
 
     }
+}}
+
 
 }
 
